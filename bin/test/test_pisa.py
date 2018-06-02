f079ed8d4ddae956f0b9352827e7053d79de03b4
Brian Jimenez <bjimenez@bsc.es>
2018-06-02 11:56:50 +0200

    Adapted PISA to the new lightdock setup

--- bin/test/test_pisa.py
+++ bin/test/test_pisa.py
@@ -3,7 +3,7 @@
 import shutil
 import os
 import filecmp
-from bin.test.regression import RegressionTest
+from regression import RegressionTest
 
 
 class TestRegressionPISAShort(RegressionTest):
@@ -21,16 +21,20 @@ class TestRegressionPISAShort(RegressionTest):
 
     def test_lightdock_2uuy_5_steps_5_glowworms(self):
         os.chdir(self.test_path)
-        num_clusters = 1
+        num_swarms = 1
         num_glowworms = 5
         steps = 5
-        command = "lightdock %s %s %d %d %d -f %s" \
-                  " -s pisa -c 1 > test_lightdock.out" % (self.test_path + '2UUY_rec.pdb',
-                                                          self.test_path + '2UUY_lig.pdb',
-                                                          num_clusters,
-                                                          num_glowworms,
-                                                          steps,
-                                                          self.golden_data_path + 'glowworm.conf')
+        
+        command = "lightdock_setup %s %s %d %d > test_lightdock.out" % ('2UUY_rec.pdb',
+                                                                        '2UUY_lig.pdb',
+                                                                        num_swarms,
+                                                                        num_glowworms
+                                                                        )
+        os.system(command)
+        command = "lightdock %s %d -c 1 -f %s -s " \
+                  "pisa >> test_lightdock.out" % (self.test_path + 'setup.json',
+                                                  steps,
+                                                  self.golden_data_path + 'glowworm.conf')
         os.system(command)
 
         assert filecmp.cmp(self.golden_data_path + 'init/initial_positions_0.dat',
@@ -39,10 +43,10 @@ class TestRegressionPISAShort(RegressionTest):
                            self.test_path + 'init/starting_positions_0.pdb')
         assert filecmp.cmp(self.golden_data_path + 'init/cluster_centers.pdb',
                            self.test_path + 'init/cluster_centers.pdb')
-        assert filecmp.cmp(self.golden_data_path + 'cluster_0/gso_0.out',
-                           self.test_path + 'cluster_0/gso_0.out')
-        assert filecmp.cmp(self.golden_data_path + 'cluster_0/gso_5.out',
-                           self.test_path + 'cluster_0/gso_5.out')
+        assert filecmp.cmp(self.golden_data_path + 'swarm_0/gso_0.out',
+                           self.test_path + 'swarm_0/gso_0.out')
+        assert filecmp.cmp(self.golden_data_path + 'swarm_0/gso_5.out',
+                           self.test_path + 'swarm_0/gso_5.out')
 
 
 class TestRegressionPISALong(RegressionTest):
@@ -62,16 +66,20 @@ class TestRegressionPISALong(RegressionTest):
     def test_lightdock_2uuy_10_steps_50_glowworms(self):
         if os.environ.has_key('LIGHTDOCK_LONG_TEST') and os.environ['LIGHTDOCK_LONG_TEST'] == 'true':
             os.chdir(self.test_path)
-            num_clusters = 1
+            num_swarms = 1
             num_glowworms = 50
             steps = 10
-            command = "lightdock %s %s %d %d %d -f %s" \
-                      " -s pisa -c 1 > test_lightdock.out" % (self.test_path + '2UUY_rec.pdb',
-                                                              self.test_path + '2UUY_lig.pdb',
-                                                              num_clusters,
-                                                              num_glowworms,
-                                                              steps,
-                                                              self.golden_data_path + 'glowworm.conf')
+            
+            command = "lightdock_setup %s %s %d %d > test_lightdock.out" % ('2UUY_rec.pdb',
+                                                                            '2UUY_lig.pdb',
+                                                                            num_swarms,
+                                                                            num_glowworms
+                                                                            )
+            os.system(command)
+            command = "lightdock %s %d -c 1 -f %s -s " \
+                      "pisa >> test_lightdock.out" % (self.test_path + 'setup.json',
+                                                      steps,
+                                                      self.golden_data_path + 'glowworm.conf')
             os.system(command)
 
             assert filecmp.cmp(self.golden_data_path + 'init/initial_positions_0.dat',
@@ -80,7 +88,7 @@ class TestRegressionPISALong(RegressionTest):
                                self.test_path + 'init/starting_positions_0.pdb')
             assert filecmp.cmp(self.golden_data_path + 'init/cluster_centers.pdb',
                                self.test_path + 'init/cluster_centers.pdb')
-            assert filecmp.cmp(self.golden_data_path + 'cluster_0/gso_0.out',
-                               self.test_path + 'cluster_0/gso_0.out')
-            assert filecmp.cmp(self.golden_data_path + 'cluster_0/gso_10.out',
-                               self.test_path + 'cluster_0/gso_10.out')
+            assert filecmp.cmp(self.golden_data_path + 'swarm_0/gso_0.out',
+                               self.test_path + 'swarm_0/gso_0.out')
+            assert filecmp.cmp(self.golden_data_path + 'swarm_0/gso_10.out',
+                               self.test_path + 'swarm_0/gso_10.out')
