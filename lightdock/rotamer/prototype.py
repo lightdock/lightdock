25848d72305ae08b9d45a7daab3211f5ce4899a3
Brian Jimenez Garcia <b.jimenezgarcia@uu.nl>
2018-09-28 11:24:58 +0200

    Revisited

--- lightdock/rotamer/prototype.py
+++ lightdock/rotamer/prototype.py
@@ -4,7 +4,7 @@ import math
 from lightdock.scoring.dfire.driver import DFIREAdapter
 from lightdock.pdbutil.PDBIO import parse_complex_from_file
 from lightdock.structure.complex import Complex
-from lightdock.rotamer.predictor import get_interface_residues, calculate_chi_angles
+from lightdock.rotamer.predictor import get_interface_residues, calculate_chi_angles, steric_energy
 from lightdock.rotamer.library import InterfaceSurfaceLibrary
 
 
@@ -55,44 +55,39 @@ if __name__ == "__main__":
         print chi_angles
         print
 
-    # print "%d receptor interface residues" % len(receptor_residue_indexes)
-    # receptor_residues = []
-    # for residue_index in receptor_residue_indexes:
-    #     residue = receptor.residues[residue_index]
-    #     chi_angles = calculate_chi_angles(residue)
-    #     for chi, angle in chi_angles.iteritems():
-    #         try:
-    #             chi_angles[chi] = math.degrees(angle)
-    #         except TypeError:
-    #             pass
-    #     index_rotamer, rotamer = rotamer_library.get_closest_rotamer(residue, chi_angles)
-    #     if chi_angles['x1']:
-    #         print residue.name, chi_angles, rotamer
-    #     receptor_residues.append(residue)
-    #
-    # print "%d ligand interface residues" % len(ligand_residue_indexes)
-    # ligand_residues = []
-    # for residue_index in ligand_residue_indexes:
-    #     residue = ligand.residues[residue_index]
-    #     chi_angles = calculate_chi_angles(residue)
-    #     for chi, angle in chi_angles.iteritems():
-    #         try:
-    #             chi_angles[chi] = math.degrees(angle)
-    #         except TypeError:
-    #             pass
-    #     index_rotamer, rotamer = rotamer_library.get_closest_rotamer(residue, chi_angles)
-    #     if chi_angles['x1']:
-    #         print residue.name, chi_angles, rotamer
-    #     ligand_residues.append(residue)
-    #
-    # # Calculate steric energy
-    # energy = 0
-    # for residue_index1 in receptor_residue_indexes:
-    #     residue1 = receptor.residues[residue_index1]
-    #     for residue_index2 in ligand_residue_indexes:
-    #         residue2 = ligand.residues[residue_index2]
-    #
-    #         for atom1 in residue1.sidechain:
-    #             for atom2 in residue2.sidechain:
-    #                 energy += steric_energy(dist_matrix[atom1.index][atom2.index], atom1.element, atom2.element)
-    # print energy
+    print "%d ligand interface residues" % len(ligand_residue_indexes)
+    ligand_residues = []
+    for residue_index in ligand_residue_indexes:
+        residue = ligand.residues[residue_index]
+        chi_angles = calculate_chi_angles(residue)
+        for chi, angle in chi_angles.iteritems():
+            try:
+                chi_angles[chi] = math.degrees(angle)
+            except TypeError:
+                pass
+        index_rotamer, rotamer = rotamer_library.get_closest_rotamer(residue, chi_angles)
+        if chi_angles['x1']:
+            print residue.name, chi_angles, rotamer
+        ligand_residues.append(residue)
+
+        # Code to move side-chain to closest rotamer and check if the angles correspond
+        residue.mutate_side_chain(rotamer)
+        chi_angles = calculate_chi_angles(residue)
+        for chi, angle in chi_angles.iteritems():
+            try:
+                chi_angles[chi] = math.degrees(angle)
+            except TypeError:
+                pass
+        print chi_angles
+        print
+    
+    # Calculate steric energy
+    energy = 0
+    for residue_index1 in receptor_residue_indexes:
+        residue1 = receptor.residues[residue_index1]
+        for residue_index2 in ligand_residue_indexes:
+            residue2 = ligand.residues[residue_index2]
+            for atom1 in residue1.sidechain:
+                for atom2 in residue2.sidechain:
+                    energy += steric_energy(dist_matrix[atom1.index][atom2.index], atom1.element, atom2.element)
+    print energy
