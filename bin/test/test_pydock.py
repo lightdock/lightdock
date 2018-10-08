0b42b9038198490b1a7848a901c13d1df20734f3
Brian Jimenez Garcia <b.jimenezgarcia@uu.nl>
2018-10-08 17:15:57 +0200

    Test for CPydock restraints

--- bin/test/test_pydock.py
+++ bin/test/test_pydock.py
@@ -84,3 +84,43 @@ class TestRegressionPyDockLong(RegressionTest):
                                self.test_path + 'swarm_0/gso_0.out')
             assert filecmp.cmp(self.golden_data_path + 'swarm_0/gso_10.out',
                                self.test_path + 'swarm_0/gso_10.out')
+
+
+class TestRegressionPyDockRestraints(RegressionTest):
+
+    def setup(self):
+        self.path = os.path.dirname(os.path.realpath(__file__))
+        self.test_path = self.path + '/scratch_pydock_restraints/'
+        self.ini_test_path()
+        self.golden_data_path = os.path.normpath(os.path.dirname(os.path.realpath(__file__))) + \
+                                '/golden_data/regression_pydock_restraints/'
+        shutil.copy(os.path.join(self.golden_data_path, '2UUY_rec.pdb'), self.test_path)
+        shutil.copy(os.path.join(self.golden_data_path, '2UUY_lig.pdb'), self.test_path)
+
+    def teardown(self):
+        self.clean_test_path()
+
+    def test_lightdock_2uuy_20_steps_25_glowworms_1_swarm(self):
+        os.chdir(self.test_path)
+        num_swarms = 4
+        num_glowworms = 25
+        steps = 20
+
+        command = "lightdock_setup %s %s %d %d -rst %s > test_lightdock.out" % ('2UUY_rec.pdb',
+                                                                                '2UUY_lig.pdb',
+                                                                                num_swarms,
+                                                                                num_glowworms,
+                                                                                self.golden_data_path + 'restraints.list'
+                                                                                )
+        os.system(command)
+        command = "lightdock %s %d -c 1 -s " \
+                  "cpydock >> test_lightdock.out" % (self.test_path + 'setup.json',
+                                                     steps)
+        os.system(command)
+
+        assert filecmp.cmp(self.golden_data_path + 'swarm_0/gso_0.out',
+                           self.test_path + 'swarm_0/gso_0.out')
+        assert filecmp.cmp(self.golden_data_path + 'swarm_0/gso_10.out',
+                           self.test_path + 'swarm_0/gso_10.out')
+        assert filecmp.cmp(self.golden_data_path + 'swarm_0/gso_20.out',
+                           self.test_path + 'swarm_0/gso_20.out')
