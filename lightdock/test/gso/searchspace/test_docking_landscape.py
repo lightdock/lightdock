ec9a4b4349c2660d7377a97e5965499008370094
Brian Jimenez Garcia <b.jimenezgarcia@uu.nl>
2020-06-19 13:17:22 +0200

    More tests for DockingLandscapePosition

--- lightdock/test/gso/searchspace/test_docking_landscape.py
+++ lightdock/test/gso/searchspace/test_docking_landscape.py
@@ -23,6 +23,38 @@ class TestDockingLandscapePosition:
 
     def tearDown(self):
         pass
+
+    def test_clone(self):
+        atoms, residues, chains = parse_complex_from_file(self.golden_data_path + '1PPElig.pdb')
+        ligand = Complex(chains, atoms)
+        adapter = MJ3hAdapter(self.receptor, ligand)
+        scoring_function = MJ3h()
+        coordinates = Coordinates([0.,0.,0.,1.,0.,0.,0.])
+        landscape_position_1 = DockingLandscapePosition(scoring_function, coordinates, 
+                                                        adapter.receptor_model, 
+                                                        adapter.ligand_model)
+        
+        landscape_position_2 = landscape_position_1.clone()
+
+        assert landscape_position_1.translation.all() == landscape_position_2.translation.all()
+        assert landscape_position_1.rotation == landscape_position_2.rotation
+
+        landscape_position_2.translation[0] = 5.0
+
+        assert_almost_equal(5.0, landscape_position_2.translation[0])
+        assert_almost_equal(0.0, landscape_position_1.translation[0])
+
+    def test_repr(self):
+        atoms, residues, chains = parse_complex_from_file(self.golden_data_path + '1PPElig.pdb')
+        ligand = Complex(chains, atoms)
+        adapter = MJ3hAdapter(self.receptor, ligand)
+        scoring_function = MJ3h()
+        coordinates = Coordinates([0.,0.,0.,1.,0.,0.,0.])
+        landscape_position = DockingLandscapePosition(scoring_function, coordinates, 
+                                                        adapter.receptor_model, 
+                                                        adapter.ligand_model)
+        expected = "( 0.0000000,  0.0000000,  0.0000000,  1.0000000,  0.0000000,  0.0000000,  0.0000000)    0    0"
+        assert expected == str(landscape_position)
     
     def test_evaluate_objective_function_no_movement(self):
         """The result of this test must be the same value as testing the MJ3h function.
@@ -175,3 +207,33 @@ class TestDockingLandscapePosition:
         
         assert (expected_translation == landscape_position1.translation).all()
         assert expected_rotation == landscape_position1.rotation
+
+    def test_move_step_rot_half_step_trans_half_and_anm(self):
+        atoms, residues, chains = parse_complex_from_file(self.golden_data_path + '1PPElig.pdb')
+        ligand = Complex(chains, atoms)
+        adapter = MJ3hAdapter(self.receptor, ligand)
+        scoring_function = MJ3h()
+        coordinates1 = Coordinates([0., 0., 0., 1., 0., 0., 0., 1., 1., 1., 1.])
+        landscape_position1 = DockingLandscapePosition(scoring_function, coordinates1, 
+                                                      adapter.receptor_model, 
+                                                      adapter.ligand_model, 
+                                                      step_translation=5.0,
+                                                      step_rotation=0.5,
+                                                      step_nmodes=0.5,
+                                                      num_rec_nmodes=2, num_lig_nmodes=2)
+        adapter2 = MJ3hAdapter(self.receptor, ligand)
+        coordinates2 = Coordinates([10., 0., 0., 0., 0., 1., 0., 0., 0., 0., 0.])
+        landscape_position2 = DockingLandscapePosition(scoring_function, coordinates2, 
+                                                      adapter2.receptor_model, 
+                                                      adapter2.ligand_model,
+                                                      num_rec_nmodes=2, num_lig_nmodes=2)
+        
+        landscape_position1.move(landscape_position2)
+        
+        expected_translation = np.array([5.,0.,0.])
+        expected_rotation = Quaternion(0.707106781, 0.0, 0.707106781, 0.0)
+        expected_anm = np.array([0.64644661, 0.64644661])
+        assert np.allclose(expected_translation, landscape_position1.translation)
+        assert expected_rotation == landscape_position1.rotation
+        assert np.allclose(expected_anm, landscape_position1.rec_extent)
+        assert np.allclose(expected_anm, landscape_position1.lig_extent)
