17a0167ba6f0835cb39bd1cb29ecab47f2301248
Brian Jimenez Garcia <b.jimenezgarcia@uu.nl>
2021-02-22 17:32:24 +0100

    Test for DNA

--- lightdock/test/bin/test_dna.py
+++ lightdock/test/bin/test_dna.py
@@ -3,78 +3,78 @@
 import shutil
 import os
 import filecmp
-
-from .regression import RegressionTest
+from pathlib import Path
+from lightdock.test.bin.regression import RegressionTest
 
 
 class TestRegressionDNAShort(RegressionTest):
 
+    def __init__(self):
+        super().__init__()
+        self.path = Path(__file__).absolute().parent
+        self.test_path = self.path / 'scratch_dna_short'
+        self.golden_data_path = self.path / 'golden_data' / 'regression_dna_short'
+
     def setup(self):
-        self.path = os.path.dirname(os.path.realpath(__file__))
-        self.test_path = self.path + '/scratch_dna_short/'
         self.ini_test_path()
-        self.golden_data_path = os.path.normpath(os.path.dirname(os.path.realpath(__file__))) + \
-                                '/golden_data/regression_dna_short/'
-        shutil.copy(os.path.join(self.golden_data_path, '1DIZ_rec.pdb.H'), self.test_path)
-        shutil.copy(os.path.join(self.golden_data_path, '1DIZ_lig.pdb.H'), self.test_path)
+        shutil.copy(self.golden_data_path / '1DIZ_rec.pdb.H', self.test_path)
+        shutil.copy(self.golden_data_path / '1DIZ_lig.pdb.H', self.test_path)
 
     def teardown(self):
         self.clean_test_path()
 
-    def test_lightdock_1diz_10_steps_20_glowworms_1_swarm(self):
+    def test_lightdock_1diz_10_steps_20_glowworms(self):
         os.chdir(self.test_path)
-        num_swarms = 1
-        num_glowworms = 20
+        num_glowworms = 25
         steps = 10
 
-        command = "lightdock3_setup.py %s %s %d %d -anm > test_lightdock.out" % ('1DIZ_rec.pdb.H',
-                                                                             '1DIZ_lig.pdb.H',
-                                                                             num_swarms,
-                                                                             num_glowworms
-                                                                             )
-        os.system(command)
-        command = "lightdock3.py %s %d -c 1 -s " \
-                  "dna >> test_lightdock.out" % (self.test_path + 'setup.json', steps)
+        command = f"lightdock3_setup.py 1DIZ_rec.pdb.H 1DIZ_lig.pdb.H -g {num_glowworms} -anm"
+        command += ">> test_lightdock.out"
         os.system(command)
 
-        assert filecmp.cmp(self.golden_data_path + 'swarm_0/gso_0.out',
-                           self.test_path + 'swarm_0/gso_0.out')
-        assert filecmp.cmp(self.golden_data_path + 'swarm_0/gso_10.out',
-                           self.test_path + 'swarm_0/gso_10.out')
+        command = f"lightdock3.py -c 1 -s dna setup.json {steps} -l 0 >> test_lightdock.out"
+        os.system(command)
+
+        assert filecmp.cmp(self.golden_data_path / 'swarm_0' / 'gso_0.out',
+                           self.test_path / 'swarm_0' / 'gso_0.out')
+        assert filecmp.cmp(self.golden_data_path / 'swarm_0' / 'gso_10.out',
+                           self.test_path / 'swarm_0' / 'gso_10.out')
 
 
-class TestRegressionDNARestraintsOnlyReceptor(RegressionTest):
+class TestRegressionDNARestraints(RegressionTest):
+
+    def __init__(self):
+        super().__init__()
+        self.path = Path(__file__).absolute().parent
+        self.test_path = self.path / 'scratch_dna_restraints'
+        self.golden_data_path = self.path / 'golden_data' / 'regression_dna_restraints'
 
     def setup(self):
-        self.path = os.path.dirname(os.path.realpath(__file__))
-        self.test_path = self.path + '/scratch_dna_restraints/'
         self.ini_test_path()
-        self.golden_data_path = os.path.normpath(os.path.dirname(os.path.realpath(__file__))) + \
-                                '/golden_data/regression_dna_restraints/'
-        shutil.copy(os.path.join(self.golden_data_path, '1DIZ_rec.pdb.H'), self.test_path)
-        shutil.copy(os.path.join(self.golden_data_path, '1DIZ_lig.pdb.H'), self.test_path)
+        shutil.copy(self.golden_data_path / '1DIZ_rec.pdb.H', self.test_path)
+        shutil.copy(self.golden_data_path / '1DIZ_lig.pdb.H', self.test_path)
+        shutil.copy(self.golden_data_path / 'restraints.list', self.test_path)
 
     def teardown(self):
         self.clean_test_path()
 
-    def test_lightdock_1diz_5_steps_10_glowworms_4_swarms(self):
+    def test_lightdock_1diz_30_steps_50_glowworms(self):
         os.chdir(self.test_path)
-        num_swarms = 4
-        num_glowworms = 10
-        steps = 5
+        num_glowworms = 50
+        steps = 30
 
-        command = "lightdock3_setup.py %s %s %d %d -rst %s > test_lightdock.out" % ('1DIZ_rec.pdb.H',
-                                                                                '1DIZ_lig.pdb.H',
-                                                                                num_swarms,
-                                                                                num_glowworms,
-                                                                                self.golden_data_path + 'restraints.list'
-                                                                                )
-        os.system(command)
-        command = "lightdock3.py %s %d -c 1 -s " \
-                  "dna >> test_lightdock.out" % (self.test_path + 'setup.json', steps)
+        command = f"lightdock3_setup.py 1DIZ_rec.pdb.H 1DIZ_lig.pdb.H -g {num_glowworms} -anm "
+        command += "-rst restraints.list >> test_lightdock.out"
         os.system(command)
 
-        assert filecmp.cmp(self.golden_data_path + 'swarm_0/gso_0.out',
-                           self.test_path + 'swarm_0/gso_0.out')
-        assert filecmp.cmp(self.golden_data_path + 'swarm_0/gso_5.out',
-                           self.test_path + 'swarm_0/gso_5.out')
+        command = f"lightdock3.py -c 1 -s dna setup.json {steps} -l 0 >> test_lightdock.out"
+        os.system(command)
+
+        assert filecmp.cmp(self.golden_data_path / 'swarm_0' / 'gso_0.out',
+                           self.test_path / 'swarm_0' / 'gso_0.out')
+        assert filecmp.cmp(self.golden_data_path / 'swarm_0' / 'gso_10.out',
+                           self.test_path / 'swarm_0' / 'gso_10.out')
+        assert filecmp.cmp(self.golden_data_path / 'swarm_0' / 'gso_20.out',
+                           self.test_path / 'swarm_0' / 'gso_20.out')
+        assert filecmp.cmp(self.golden_data_path / 'swarm_0' / 'gso_30.out',
+                           self.test_path / 'swarm_0' / 'gso_30.out')
