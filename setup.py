7180a73e795dc9ad02dc143cad182f0e3c21c867
Brian Jimenez Garcia <b.jimenezgarcia@uu.nl>
2021-02-23 16:06:29 +0100

    Setup version, ready to pack

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ exts = [Extension(name='lightdock.mathutil.cython.cutil',
 
 setuptools.setup(
     name='lightdock',
-    version='0.9.0',
+    version='0.9.0a1',
     description="A macromolecular docking framework",
     long_description=long_description,
     long_description_content_type="text/markdown",
