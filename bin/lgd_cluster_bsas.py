51c148c7df5d891b2b656c5733e72350f5447647
Brian Jimenez Garcia <b.jimenezgarcia@uu.nl>
2021-02-24 10:55:16 +0100

    Implements a fix for #11

--- bin/lgd_cluster_bsas.py
+++ bin/lgd_cluster_bsas.py
@@ -3,6 +3,7 @@
 """Cluster LightDock final swarm results using BSAS algorithm"""
 
 import argparse
+from pathlib import Path
 from prody import parsePDB, confProDy, calcRMSD, matchChains
 from lightdock.util.analysis import read_lightdock_output
 from lightdock.util.logger import LoggingManager
@@ -23,7 +24,7 @@ def parse_command_line():
     return parser.parse_args()
 
 
-def get_backbone_atoms(ids_list):
+def get_backbone_atoms(ids_list, swarm_path):
     """Get all backbone atoms (CA or P) of the PDB files specified by the ids_list.
 
     PDB files follow the format lightdock_ID.pdb where ID is in ids_list
@@ -31,7 +32,7 @@ def get_backbone_atoms(ids_list):
     ca_atoms = {}
     try:
         for struct_id in ids_list:
-            pdb_file = f'lightdock_{struct_id}.pdb'
+            pdb_file = swarm_path / f'lightdock_{struct_id}.pdb'
             log.info(f'Reading CA from {pdb_file}')
             structure = parsePDB(pdb_file)
             selection = structure.select('name CA P')
@@ -43,14 +44,14 @@ def get_backbone_atoms(ids_list):
     return ca_atoms
 
 
-def clusterize(sorted_ids):
+def clusterize(sorted_ids, swarm_path):
     """Clusters the structures identified by the IDS inside sorted_ids list"""
 
     clusters_found = 0
     clusters = {clusters_found: [sorted_ids[0]]}
 
     # Read all structures backbone atoms
-    backbone_atoms = get_backbone_atoms(sorted_ids)
+    backbone_atoms = get_backbone_atoms(sorted_ids, swarm_path)
 
     for j in sorted_ids[1:]:
         log.info("Glowworm %d with pdb lightdock_%d.pdb" % (j, j))
@@ -73,13 +74,14 @@ def clusterize(sorted_ids):
     return clusters
 
 
-def write_cluster_info(clusters, gso_data):
+def write_cluster_info(clusters, gso_data, swarm_path):
     """Writes the clustering result"""
-    with open(CLUSTER_REPRESENTATIVES_FILE, 'w') as output:
+    file_name = swarm_path / CLUSTER_REPRESENTATIVES_FILE
+    with open(file_name, 'w') as output:
         for id_cluster, ids in clusters.items():
             output.write("%d:%d:%8.5f:%d:%s\n" % (id_cluster, len(ids), gso_data[ids[0]].scoring,
                                                   ids[0], 'lightdock_%d.pdb' % ids[0]))
-        log.info("Cluster result written to %s file" % CLUSTER_REPRESENTATIVES_FILE)
+        log.info(f"Cluster result written to {file_name} file")
 
 
 if __name__ == '__main__':
@@ -98,10 +100,11 @@ if __name__ == '__main__':
         sorted_ids = [g.id_glowworm for g in sorted_data]
 
         # Calculate the different clusters
-        clusters = clusterize(sorted_ids)
+        swarm_path = Path(args.gso_output_file).absolute().parent
+        clusters = clusterize(sorted_ids, swarm_path)
 
         # Write clustering information
-        write_cluster_info(clusters, gso_data)
+        write_cluster_info(clusters, gso_data, swarm_path)
 
     except Exception as e:
         log.error('Clustering has failed. Please see error:')
