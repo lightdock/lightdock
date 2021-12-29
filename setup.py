3cb6e4baed868702c296a3adf837768c0f0f1ec0
Brian Jimenez Garcia <b.jimenezgarcia@uu.nl>
2021-12-29 12:08:09 +0100

    Updated ProDy version to account for #20

--- setup.py
+++ setup.py
@@ -182,7 +182,7 @@ setuptools.setup(
         "numpy>=1.17.1",
         "scipy>=1.3.1",
         "cython>=0.29.13",
-        "prody>=1.10.11",
+        "prody>=2.0.1",
         "freesasa>=2.0.3",
     ],
     scripts=[
