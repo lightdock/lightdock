9ee75064c0f02958176951aade0fc94f0f737c13
Brian Jimenez <br.jimenezgarcia@gmail.com>
2023-08-08 17:08:08 +0200

    Implements pdbutil tests for #59

--- lightdock/test/pdbutil/test_PDBIO.py
+++ lightdock/test/pdbutil/test_PDBIO.py
@@ -1,11 +1,8 @@
 """Tests for PDBReader module"""
 
-import shutil
-import os
+import pytest
 import filecmp
 from pathlib import Path
-from nose.tools import assert_almost_equals
-from nose.tools import raises
 from lightdock.pdbutil.PDBIO import (
     read_atom_line,
     parse_complex_from_file,
@@ -16,59 +13,45 @@ from lightdock.error.lightdock_errors import PDBParsingError
 
 
 class TestPDBReader:
-    def __init__(self):
+    def setup_class(self):
         self.path = Path(__file__).absolute().parent
-        self.test_path = self.path / "scratch_pdbio"
         self.golden_data_path = self.path / "golden_data"
 
-    def setup(self):
-        try:
-            shutil.rmtree(self.test_path)
-        except OSError:
-            pass
-        os.mkdir(self.test_path)
-
-    def teardown(self):
-        try:
-            shutil.rmtree(self.test_path)
-        except OSError:
-            pass
-
-    @raises(PDBParsingError)
     def test_read_wrong_atom_line(self):
-        line = "ATOM     11  NH2 ARG A   1       2.559  16.752     AA   1.00 14.90           N"
-        read_atom_line(line, "ATOM")
-        assert False
+        with pytest.raises(PDBParsingError):
+            line = "ATOM     11  NH2 ARG A   1       2.559  16.752     AA   1.00 14.90           N"
+            read_atom_line(line, "ATOM")
+            assert False
 
-    @raises(PDBParsingError)
     def test_read_nan_in_atom_line(self):
-        line = "ATOM     11  NH2 ARG A   1       2.559  16.752     NaN  1.00 14.90           N"
-        read_atom_line(line, "ATOM")
-        assert False
+        with pytest.raises(PDBParsingError):
+            line = "ATOM     11  NH2 ARG A   1       2.559  16.752     NaN  1.00 14.90           N"
+            read_atom_line(line, "ATOM")
+            assert False
 
-    @raises(PDBParsingError)
     def test_wrong_atom_number(self):
-        line = "ATOM     OO  NH2 ARG A   1       2.559  16.752    1.00  1.00 14.90           N"
-        read_atom_line(line, "ATOM")
-        assert False
+        with pytest.raises(PDBParsingError):
+            line = "ATOM     OO  NH2 ARG A   1       2.559  16.752    1.00  1.00 14.90           N"
+            read_atom_line(line, "ATOM")
+            assert False
 
-    @raises(PDBParsingError)
     def test_wrong_residue_number(self):
-        line = "ATOM     12  NH2 ARG A   A       2.559  16.752    1.00  1.00 14.90           N"
-        read_atom_line(line, "ATOM")
-        assert False
+        with pytest.raises(PDBParsingError):
+            line = "ATOM     12  NH2 ARG A   A       2.559  16.752    1.00  1.00 14.90           N"
+            read_atom_line(line, "ATOM")
+            assert False
 
     def test_default_occupancy(self):
         line = "ATOM     12  NH2 ARG A   1       2.559  16.752    1.00       14.90           N"
         atom = read_atom_line(line, "ATOM")
 
-        assert_almost_equals(1.0, atom.occupancy)
+        assert 1.0 == pytest.approx(atom.occupancy)
 
     def test_default_b_factor(self):
         line = "ATOM     12  NH2 ARG A   1       2.559  16.752    1.00  1.00                 N"
         atom = read_atom_line(line, "ATOM")
 
-        assert_almost_equals(0.0, atom.b_factor)
+        0.0 == pytest.approx(atom.b_factor)
 
     def test_default_no_line_type(self):
         line = "ATOM     12  NH2 ARG A   1       2.559  16.752    1.00  1.00                 N"
@@ -94,7 +77,7 @@ class TestPDBReader:
         assert len(residues) == 1
         assert len(chains) == 1
 
-    def test_write_pdb_to_file(self):
+    def test_write_pdb_to_file(self, tmp_path):
         atoms, _, chains = parse_complex_from_file(
             self.golden_data_path / "1PPE_l_u.pdb"
         )
@@ -102,15 +85,15 @@ class TestPDBReader:
         assert atoms == protein.atoms
 
         write_pdb_to_file(
-            protein, self.test_path / "parsed_1PPE_l_u.pdb", protein.atom_coordinates[0]
+            protein, tmp_path / "parsed_1PPE_l_u.pdb", protein.atom_coordinates[0]
         )
 
         assert filecmp.cmp(
             self.golden_data_path / "parsed_1PPE_l_u.pdb",
-            self.test_path / "parsed_1PPE_l_u.pdb",
+            tmp_path / "parsed_1PPE_l_u.pdb",
         )
 
-    def test_parse_pdb_noh(self):
+    def test_parse_pdb_noh(self, tmp_path):
         atoms_to_ignore = ["H"]
         atoms, _, chains = parse_complex_from_file(
             self.golden_data_path / "1PPE_lig_with_H.pdb", atoms_to_ignore
@@ -120,11 +103,11 @@ class TestPDBReader:
 
         write_pdb_to_file(
             protein,
-            self.test_path / "parsed_1PPE_lig_with_H.pdb",
+            tmp_path / "parsed_1PPE_lig_with_H.pdb",
             protein.atom_coordinates[0],
         )
 
         assert filecmp.cmp(
             self.golden_data_path / "parsed_1PPE_lig_with_H.pdb",
-            self.test_path / "parsed_1PPE_lig_with_H.pdb",
+            tmp_path / "parsed_1PPE_lig_with_H.pdb",
         )
