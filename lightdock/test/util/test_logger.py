dea05542b1ba56a2279bc32b01a4ccde710be4c9
Brian Jimenez <br.jimenezgarcia@gmail.com>
2023-08-08 13:39:56 +0200

    Implements util tests for #59

--- lightdock/test/util/test_logger.py
+++ lightdock/test/util/test_logger.py
@@ -1,31 +1,17 @@
 """Tests for Logger module"""
-import shutil
-import os
-import filecmp
+
 import sys
+import pytest
+import filecmp
 from pathlib import Path
 from io import StringIO
 from lightdock.util.logger import Logger
 
 
 class TestLogger:
-    def __init__(self):
+    def setup_class(self):
         self.path = Path(__file__).absolute().parent
-        self.test_path = self.path / "scratch_logger"
         self.golden_data_path = self.path / "golden_data"
-
-    def setup(self):
-        try:
-            shutil.rmtree(self.test_path)
-        except OSError:
-            pass
-        os.mkdir(self.test_path)
-
-    def teardown(self):
-        try:
-            shutil.rmtree(self.test_path)
-        except OSError:
-            pass
 
     def test_create_default_logger(self):
         log = Logger("test")
@@ -45,37 +31,37 @@ class TestLogger:
         log.set_level("-1")
         assert Logger.DEBUG == log.get_level()
 
-    def test_create_logger_with_file(self):
-        log = Logger("test", file_name=self.test_path / "test_file.log")
+    def test_create_logger_with_file(self, tmp_path):
+        log = Logger("test", file_name=tmp_path / "test_file.log")
         log.info("Testing")
 
-        assert (self.test_path / "test_file.log").exists()
+        assert (tmp_path / "test_file.log").exists()
 
-    def test_output_debug(self):
-        log = Logger("test", file_name=self.test_path / "test_file.log")
+    def test_output_debug(self, tmp_path):
+        log = Logger("test", file_name=tmp_path / "test_file.log")
         log.debug("Testing DEBUG")
         log.set_level(Logger.DEBUG)
         log.debug("Testing DEBUG")
 
         assert filecmp.cmp(
-            self.golden_data_path / "output_debug.log", self.test_path / "test_file.log"
+            self.golden_data_path / "output_debug.log", tmp_path / "test_file.log"
         )
 
-    def test_output_warning(self):
-        log = Logger("test", file_name=self.test_path / "test_file.log")
+    def test_output_warning(self, tmp_path):
+        log = Logger("test", file_name=tmp_path / "test_file.log")
         log.warning("Testing WARNING")
 
         assert filecmp.cmp(
             self.golden_data_path / "output_warning.log",
-            self.test_path / "test_file.log",
+            tmp_path / "test_file.log",
         )
 
-    def test_output_error(self):
-        log = Logger("test", file_name=self.test_path / "test_file.log")
+    def test_output_error(self, tmp_path):
+        log = Logger("test", file_name=tmp_path / "test_file.log")
         log.error("Testing ERROR")
 
         assert filecmp.cmp(
-            self.golden_data_path / "output_error.log", self.test_path / "test_file.log"
+            self.golden_data_path / "output_error.log", tmp_path / "test_file.log"
         )
 
     def test_output_std_out(self):
